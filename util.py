c4abac5b3e121a0303349498e7438fcc2216b643
RicardoL1u <2018212874@bupt.edu.cn>
2022-12-10 18:08:39 +0800

    ref: yaml load to sale_load

--- util.py
+++ util.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 
 def get_config(config_path="config.yml"):
     with open(config_path, "r") as setting:
-        config = yaml.load(setting)
+        config = yaml.safe_load(setting)
     return config
 
 def use_cuda(var):
